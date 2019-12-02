8d18c4aa0644f1ab2ab0795e7ba72b470fdd7db3
Irina Agafonova <la_caterina@mail.ru>
2019-12-02 16:30:00 +0300

    wall done

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+x, y = 0, 0
+step_y = 50
+step_x = 100
+wall_length = 600
+i = 0
 
-simple_draw.pause()
+for i, y in enumerate(range(y, wall_length, step_y)):
+    i += 1
+    for x in range(0, wall_length, step_x):
+        if i % 2 == 0:
+            point_start = sd.get_point(step_x - step_x, y)
+            point_end1 = sd.get_point(x + step_x, y + step_y)
+            sd.rectangle(left_bottom=point_start, right_top=point_end1, color=sd.COLOR_ORANGE, width=1)
+        else:
+            point_start = sd.get_point(x - 50, y)
+            point_end = sd.get_point(x + (step_x * 0.5), y + step_y)
+            sd.rectangle(left_bottom=point_start, right_top=point_end, color=sd.COLOR_ORANGE, width=1)
+
+
+sd.pause()
