b48de618256da6a7d791d7281578844c03e2acb9
Irina Agafonova <la_caterina@mail.ru>
2020-01-09 22:01:28 +0300

    bubbles done

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -10,16 +10,20 @@ radius = 50
 for _ in range(3):
     radius += 5
     sd.circle(center_position=point, radius=radius, width=2)
+# Написать функцию рисования пузырька, принимающую 2 (или более) параметра: точка рисовании и шаг
+colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE,
+          sd.COLOR_DARK_YELLOW, sd.COLOR_DARK_BLUE, sd.COLOR_DARK_CYAN, sd.COLOR_DARK_GREEN, sd.COLOR_DARK_GREEN,
+          sd.COLOR_DARK_ORANGE, sd.COLOR_DARK_PURPLE, sd.COLOR_DARK_RED)
 
-# Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO используйте Code/Reformat code, чтобы исправить ошибки стиля
-def bubble(point, step):  # TODO Кроме прочего здесь нужен параметр для передачи цвета
+
+def bubble(point, step):
     radius = 50
     for _ in range(3):
         radius += step
-        sd.circle(center_position=point, radius=radius, width=2)  # TODO А сюда его надо будет передавать
+        sd.circle(center_position=point, radius=radius, width=2, color=colors)
 
-point = sd.get_point(300, 300)
+
+'''point = sd.get_point(300, 300)
 bubble(point=point, step=10)
 
 # Нарисовать 10 пузырьков в ряд
@@ -31,14 +35,14 @@ for x in range(100, 1100, 100):
 for y in range(100, 301, 100):
     for x in range(100, 1100, 100):
         point = sd.get_point(x, y)
-        bubble(point=point, step=5)
+        bubble(point=point, step=5)'''
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
+
 for _ in range(100):
     point = sd.random_point()
     step = random.randint(2, 10)
+    colors = sd.random_color()
     bubble(point=point, step=step)
 
 sd.pause()
-
-
