b305b88a5f27670169f31244916fee65409d4dd0
Irina Agafonova <la_caterina@mail.ru>
2020-01-22 22:41:19 +0300

    wall done

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -15,15 +15,14 @@ wall_length = 600
 
 for i, y in enumerate(range(y, wall_length, step_y)):
     for x in range(0, wall_length, step_x):
-        if i % 2 == 0:  # TODO Хорошо, но обратите внимание, что изменений на самом деле не так много
-            point_start = sd.get_point(x0, y)  # TODO Эту строку и следующие
-            point_end1 = sd.get_point(x + step_x, y + step_y)
-            sd.rectangle(left_bottom=point_start, right_top=point_end1, color=sd.COLOR_ORANGE, width=1)
+        if i % 2 == 0:
+            x0 = 0
         else:
-            point_start = sd.get_point(x0 - 50, y)
-            point_end = sd.get_point(x + (step_x * 0.5), y + step_y)  # TODO Можно сделать одинаковыми
-            # TODO И поставить после блока if/else
-            # TODO А внутри if/else - изменять только x0
-            sd.rectangle(left_bottom=point_start, right_top=point_end, color=sd.COLOR_ORANGE, width=1)
+            x0 = x - 50
+            x = x + (step_x * 0.5)
+        point_start = sd.get_point(x, y)
+        point_end = sd.get_point(x + step_x, y + step_y)
+        sd.rectangle(left_bottom=point_start, right_top=point_end, color=sd.COLOR_ORANGE, width=1)
+
 
 sd.pause()
