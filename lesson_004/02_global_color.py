08571ba23dd98916faf87121a55ef88c3085e678
Irina Agafonova <la_caterina@mail.ru>
2020-03-05 22:19:44 +0300

    global color done

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -13,107 +13,69 @@ import simple_draw as sd
 # sd.get_vector()
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
-COLOR_RED = (255, 0, 0)
-COLOR_ORANGE = (255, 127, 0)
-COLOR_YELLOW = (255, 255, 0)
-COLOR_GREEN = (0, 255, 0)
-COLOR_CYAN = (0, 255, 255)
-COLOR_BLUE = (0, 0, 255)
-COLOR_PURPLE = (255, 0, 255)
-
-color = [COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE]
-
-print('Возможные цвета:', '0 : red', '1 : orange', '2 : yellow', '3 : green', '4 : cyan', '5 : blue', '6 : purple', sep='\n')
-# TODO В этом случае удобнее создать словарь следующей структуры
-# TODO словарь = {'0': {'color_name': 'red', 'sd_name': sd.COLOR_RED},...}
-# TODO Таким образом для каждого цвета у нас будет свой словарь. И у каждого словаря будут одинаковые ключи
-# TODO 'color_name' и 'sd_name'
-# TODO Тогда можно будет легко проверить ввод (user_input in словарь)
-# TODO А если среди ключей есть выбор пользователя - по этому ключу мы получим нужный вложенный словарь
-# TODO А там все ключи одинаковые, можем получить как название цвета, так и sd_цвет
+colors = {
+    '0': {'color_name': 'red', 'sd_name': sd.COLOR_RED},
+    '1': {'color_name': 'orange', 'sd_name': sd.COLOR_ORANGE},
+    '2': {'color_name': 'yellow', 'sd_name': sd.COLOR_YELLOW},
+    '3': {'color_name': 'green', 'sd_name': sd.COLOR_GREEN},
+    '4': {'color_name': 'cyan', 'sd_name': sd.COLOR_CYAN},
+    '5': {'color_name': 'blue', 'sd_name': sd.COLOR_BLUE},
+    '6': {'color_name': 'purple', 'sd_name': sd.COLOR_PURPLE}
+}
+print('Возможные цвета:', '0 : ' + colors['0']['color_name'], '1 : ' + colors['1']['color_name'],
+      '2 : ' + colors['2']['color_name'], '3 : ' + colors['3']['color_name'], '4 : ' + colors['4']['color_name'],
+      '5 : ' + colors['5']['color_name'], '6 : ' + colors['6']['color_name'], sep='\n')
 
 while True:
     user_input = input('Введите желаемый цвет >')
-    user_input = int(user_input)
-    if 0 <= user_input <= 6:
+    if user_input in colors:
+        color = colors[user_input]['sd_name']
         break
     else:
         print("Вы ввели некорректный номер")
 
-# TODO все def надо перенести в начало файла
-def triangle(point, angle=25, length=100):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color[user_input])
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
-    v2.draw(color=color[user_input])
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
-    v3.draw(color=color[user_input])
+def draw_figure(point, angle, length):
+    for angle in range(0, 360 - angle, angle):
+        v = sd.get_vector(start_point=point, angle=angle + 25, length=length, width=3)
+        v.draw(color=color)
+        point = v.end_point
+    sd.line(start_point=point, end_point=point_0, color=color, width=3)
 
 
-point_0 = sd.get_point(100, 100)
-triangle(point=point_0, angle=25, length=100)
+def triangle(point, angle, length):
+    draw_figure(point=point, angle=angle, length=length)
 
 
-def square(point, angle=25, length=100):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color[user_input])
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
-    v2.draw(color=color[user_input])
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
-    v3.draw(color=color[user_input])
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length=length, width=3)
-    v4.draw(color=color[user_input])
+point_0 = sd.get_point(150, 130)
+side = 3
+triangle(point=point_0, angle=360 // side, length=100)
 
 
-point_0 = sd.get_point(400, 100)
-square(point=point_0, angle=25, length=100)
+def square(point, angle=25, length=100, width=3):
+    draw_figure(point=point, angle=angle, length=length)
 
 
-def pentagon(point, angle=25, length=100):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color[user_input])
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
-    v2.draw(color=color[user_input])
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
-    v3.draw(color=color[user_input])
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
-    v4.draw(color=color[user_input])
-
-    sd.line(start_point=v4.end_point, end_point=point, color=color[user_input], width=3)
+point_0 = sd.get_point(400, 130)
+side = 4
+square(point=point_0, angle=360//side, length=100)
 
 
-point_0 = sd.get_point(100, 350)
-pentagon(point=point_0, angle=25, length=100)
+def pentagon(point, angle=25, length=100, width=3):
+    draw_figure(point=point, angle=angle, length=length)
 
 
-def hexagon(point, angle=25, length=100):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw(color=color[user_input])
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
-    v2.draw(color=color[user_input])
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
-    v3.draw(color=color[user_input])
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
-    v4.draw(color=color[user_input])
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
-    v5.draw(color=color[user_input])
-
-    sd.line(start_point=v5.end_point, end_point=point, color=color[user_input], width=3)
+point_0 = sd.get_point(150, 350)
+side = 5
+pentagon(point=point_0, angle=360//side, length=100)
 
 
-point_0 = sd.get_point(400, 350)
-hexagon(point=point_0, angle=25, length=100)
+def hexagon(point, angle, length=100, width=3):
+    draw_figure(point=point, angle=angle, length=length)
+
+
+point_0 = sd.get_point(450, 350)
+side = 6
+hexagon(point=point_0, angle=360//side, length=100)
 
 sd.pause()
