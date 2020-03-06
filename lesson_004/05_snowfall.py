b30e463e91161fe675f7bfb47edf325c7dadb5c2
Irina Agafonova <la_caterina@mail.ru>
2020-03-06 22:30:07 +0300

    snowfall done

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -20,45 +20,29 @@ N = 20
 
 snowflakes = []
 snowdrift = []
-k = random.randint(2, 3)
+k = random.randint(-1, 2)
 
-for _ in range(20):
+for _ in range(N):
     x = sd.random_number(5, 550)
     y = sd.random_number(650, 670)
     size = sd.random_number(10, 30)
     snowflakes.append([x, y, size])
-print(snowflakes)
 
-snowflake = 0
-
+sd.start_drawing()
 while True:
-    for x, y, size in snowflakes:
-        # TODO Лучшей практикой будет for i, y in enumerate(список):
-        # TODO Так у вас будет доступ и к индексам (i) и к объектам списка
-        # TODO Индексы нужны потому, что при обращении к x, y, size вы изменяете только эти переменные
-        # TODO а значения в списке остаются неизменными.
-        # TODO Чтобы изменить координату в списке придётся обращаться к ней по индексу снежинки[индекс] -= 35 например
-        snowflake += 1
-        sd.start_drawing()  # TODO Эту строку надо написать до цикла, вызывать её многократно не стоит
-        # TODO Это как бы метка "отсюда", указывающаяя откуда надо начать рисовать и до finish...
-        point = sd.get_point(x, y)
-        print(point)
-        sd.snowflake(center=point, color=sd.background_color, length=size)
-        y -= 35 * k
-        # if y < 20:
+    for i, y in enumerate(snowflakes):
+        point = sd.get_point(snowflakes[i][0], snowflakes[i][1])
+        sd.snowflake(center=point, color=sd.background_color, length=snowflakes[i][2])
+        snowflakes[i][1] -= 35 * k
+        if snowflakes[i][1] < 20:
+            break
+            snowdrift.append(snowflakes[i][0], snowflakes[i][1],snowflakes[i][2])
+        snowflakes[i][0] += 5 * k
+        point = sd.get_point(snowflakes[i][0], snowflakes[i][1])
+        sd.snowflake(center=point, color=sd.COLOR_WHITE, length=snowflakes[i][2])
+        # if i > 19:
         #     break
-        x += 5
-        point = sd.get_point(x, y)
-        print(point)
-        sd.snowflake(center=point, color=sd.COLOR_WHITE, length=size)
-        snowflakes.append([x, y, size])
-        # TODO Хм, интересное решение) впервые вижу такой ход
-        # TODO Вам тогда по идее и for не нужен.
-        # TODO Однако затратный он по ресурсам очень. Ведь список расшиярется и делает это бесконечно.
-        # TODO Давайте попробуем использовать только 20 координат, добавленных до начала
-        print(snowflakes)
 
-        # TODO Эту вспомогательную часть надо писать после цикла
         sd.finish_drawing()
         sd.sleep(0.1)
         if sd.user_want_exit():
