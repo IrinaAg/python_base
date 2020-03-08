29398f25b3c6f78d1710f7790c573fe4dadd3ab8
Irina Agafonova <la_caterina@mail.ru>
2020-03-08 11:43:38 +0300

    shape select done

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -2,15 +2,15 @@
 
 import simple_draw as sd
 
+
 # Запросить у пользователя желаемую фигуру посредством выбора из существующих
 #   вывести список всех фигур с номерами и ждать ввода номера желаемой фигуры.
 # и нарисовать эту фигуру в центре экрана
 
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
-print('Возможные фигуры:', '0 : треугольник', '1 : квадрат', '2 : пятиугольник', '3 : шестиугольник', sep='\n')
 
-# TODO все def надо держать в верхенй части программы, а остальной код после всех def
+
 def draw_figure(point, angle, length):
     for angle in range(0, 360 - angle, angle):
         v = sd.get_vector(start_point=point, angle=angle + 25, length=length, width=3)
@@ -19,49 +19,43 @@ def draw_figure(point, angle, length):
     sd.line(start_point=point, end_point=point_0, width=3)
 
 
-point_0 = sd.get_point(300, 250)
-
-
 def triangle(point, angle, length):
-    draw_figure(point=point, angle=360//3, length=length)
-
-
-side = 3
+    draw_figure(point=point, angle=360 // 3, length=length)
 
 
 def square(point, angle, length):
-    draw_figure(point=point, angle=360//4, length=length)
-
-
-side = 4
+    draw_figure(point=point, angle=360 // 4, length=length)
 
 
 def pentagon(point, angle, length):
-    draw_figure(point=point, angle=360//5, length=length)
-
-
-side = 5
+    draw_figure(point=point, angle=360 // 5, length=length)
 
 
 def hexagon(point, angle, length):
-    draw_figure(point=point, angle=360//6, length=length)
+    draw_figure(point=point, angle=360 // 6, length=length)
 
 
+point_0 = sd.get_point(300, 250)
+side = 3
+side = 4
+side = 5
 side = 6
-
 
 functions = {
     '0': {'func_name': 'треугольник', 'func': triangle},
     '1': {'func_name': 'квадрат', 'func': square},
     '2': {'func_name': 'пятиугольник', 'func': pentagon},
-    '3': {'funct_name': 'шестиугольник', 'func': hexagon}
+    '3': {'func_name': 'шестиугольник', 'func': hexagon}
 }
+print('Возможные фигуры:')
+for code, val in functions.items():
+    print(code, ':', val['func_name'])
 
 while True:
     user_input = input('Введите желаемую фигуру >')
     if user_input in functions:
         function = functions[user_input]['func']
-        function(point=point_0, angle=360//side, length=100)
+        function(point=point_0, angle=360 // side, length=100)
         break
     else:
         print("Вы ввели некорректный номер")
