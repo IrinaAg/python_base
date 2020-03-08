6c9963fc3b5a5366a483aa722bf5fe280742b862
Irina Agafonova <la_caterina@mail.ru>
2020-03-08 11:23:45 +0300

    global color done

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
 
+
 # Добавить цвет в функции рисования геом. фигур. из упр lesson_004/01_shapes.py
 # (код функций скопировать сюда и изменить)
 # Запросить у пользователя цвет фигуры посредством выбора из существующих:
@@ -13,29 +14,6 @@ import simple_draw as sd
 # sd.get_vector()
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
-# TODO все def надо держать в верхенй части программы, а остальной код после всех def
-colors = {
-    '0': {'color_name': 'red', 'sd_name': sd.COLOR_RED},
-    '1': {'color_name': 'orange', 'sd_name': sd.COLOR_ORANGE},
-    '2': {'color_name': 'yellow', 'sd_name': sd.COLOR_YELLOW},
-    '3': {'color_name': 'green', 'sd_name': sd.COLOR_GREEN},
-    '4': {'color_name': 'cyan', 'sd_name': sd.COLOR_CYAN},
-    '5': {'color_name': 'blue', 'sd_name': sd.COLOR_BLUE},
-    '6': {'color_name': 'purple', 'sd_name': sd.COLOR_PURPLE}
-}
-# TODO Этот принт не стоило формировать вручную - лучше использовать цикл
-# TODO Тогда, если мы добавим ещё один цвет в словарь - не нужно будет менять принт вручную
-print('Возможные цвета:', '0 : ' + colors['0']['color_name'], '1 : ' + colors['1']['color_name'],
-      '2 : ' + colors['2']['color_name'], '3 : ' + colors['3']['color_name'], '4 : ' + colors['4']['color_name'],
-      '5 : ' + colors['5']['color_name'], '6 : ' + colors['6']['color_name'], sep='\n')
-
-while True:
-    user_input = input('Введите желаемый цвет >')
-    if user_input in colors:
-        color = colors[user_input]['sd_name']
-        break
-    else:
-        print("Вы ввели некорректный номер")
 
 
 def draw_figure(point, angle, length):
@@ -50,35 +28,54 @@ def triangle(point, angle, length):
     draw_figure(point=point, angle=angle, length=length)
 
 
-point_0 = sd.get_point(150, 130)
-side = 3
-triangle(point=point_0, angle=360 // side, length=100)
-
-
 def square(point, angle=25, length=100, width=3):
     draw_figure(point=point, angle=angle, length=length)
-
-
-point_0 = sd.get_point(400, 130)
-side = 4
-square(point=point_0, angle=360//side, length=100)
 
 
 def pentagon(point, angle=25, length=100, width=3):
     draw_figure(point=point, angle=angle, length=length)
 
 
-point_0 = sd.get_point(150, 350)
-side = 5
-pentagon(point=point_0, angle=360//side, length=100)
-
-
 def hexagon(point, angle, length=100, width=3):
     draw_figure(point=point, angle=angle, length=length)
 
 
+colors = {
+    '0': {'color_name': 'red', 'sd_name': sd.COLOR_RED},
+    '1': {'color_name': 'orange', 'sd_name': sd.COLOR_ORANGE},
+    '2': {'color_name': 'yellow', 'sd_name': sd.COLOR_YELLOW},
+    '3': {'color_name': 'green', 'sd_name': sd.COLOR_GREEN},
+    '4': {'color_name': 'cyan', 'sd_name': sd.COLOR_CYAN},
+    '5': {'color_name': 'blue', 'sd_name': sd.COLOR_BLUE},
+    '6': {'color_name': 'purple', 'sd_name': sd.COLOR_PURPLE}
+}
+
+print('Возможные цвета:')
+for code, val in colors.items():
+    print(code, ':', val['color_name'])
+
+while True:
+    user_input = input('Введите желаемый цвет >')
+    if user_input in colors:
+        color = colors[user_input]['sd_name']
+        break
+    else:
+        print("Вы ввели некорректный номер")
+
+point_0 = sd.get_point(150, 130)
+side = 3
+triangle(point=point_0, angle=360 // side, length=100)
+
+point_0 = sd.get_point(400, 130)
+side = 4
+square(point=point_0, angle=360 // side, length=100)
+
+point_0 = sd.get_point(150, 350)
+side = 5
+pentagon(point=point_0, angle=360 // side, length=100)
+
 point_0 = sd.get_point(450, 350)
 side = 6
-hexagon(point=point_0, angle=360//side, length=100)
+hexagon(point=point_0, angle=360 // side, length=100)
 
 sd.pause()
