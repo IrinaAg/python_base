5137d40939ad5d8f3808b9919da5bc9e3e76169e
Irina Agafonova <la_caterina@mail.ru>
2020-03-08 21:55:26 +0300

    snowfall done

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -3,7 +3,6 @@
 import simple_draw as sd
 import random
 
-
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
@@ -19,43 +18,36 @@ N = 20
 # sd.user_want_exit()
 
 snowflakes = []
-snowdrift = []
 k = random.randint(-1, 2)
 
 for _ in range(N):
     x = sd.random_number(5, 550)
-    y = sd.random_number(650, 670)
+    y = sd.random_number(620, 650)
     size = sd.random_number(10, 30)
     snowflakes.append([x, y, size])
 
-sd.start_drawing()  # TODO Эту строку надо добавить перед циклом с рисованием (перед for, внутри while)
 while True:
+    sd.start_drawing()
     for i, y in enumerate(snowflakes):
         point = sd.get_point(snowflakes[i][0], snowflakes[i][1])
         sd.snowflake(center=point, color=sd.background_color, length=snowflakes[i][2])
         snowflakes[i][1] -= 35 * k
-        if snowflakes[i][1] < 20:
-            break  # TODO До добавления снежинок не доходит из-за этого break
-            # TODO Кроме того - с упавшей снежинкой тоже надо что-то делать
-            # TODO Либо удалять её (опишу ниже это), либо не изменять координаты упавших снежинок
-            snowdrift.append(snowflakes[i][0], snowflakes[i][1],snowflakes[i][2])
         snowflakes[i][0] += 5 * k
         point = sd.get_point(snowflakes[i][0], snowflakes[i][1])
         sd.snowflake(center=point, color=sd.COLOR_WHITE, length=snowflakes[i][2])
-        # if i > 19:
-        #     break
+        if snowflakes[i][1] < 20:
+            snowflakes.append(i)
+            snowflakes.reverse()
+            for index in range(N):
+                snowflakes.pop()
+                # print(snowflakes)
         # TODO Эту часть кода ниже надо писать после цикла, а не в нем (измените отступ)
+        # TODO если убираю отступ снежинки падают все сразу, не поочереди
         sd.finish_drawing()
         sd.sleep(0.1)
         if sd.user_want_exit():
             break
-# TODO Упавшие снежинки можно либо запускать вновь в полет
-# TODO Либо пойти по сложному пути и в этом цикле сохранять индексы упавших снежинок
-# TODO А затем ещё одним циклом удалять снежинки из списка
-# TODO Только список надо будет перевернуть (можно с помощью reversed())
-# TODO Это нужно чтобы удалять снежинки с конца, а не с начала
-# TODO А это, в свою очередь, нужно из-за сдвига, который образуется после удаления элемента списка
-# TODO Удалив элемент под индексом 0, все последующие сдвинутся на -1 индекс
+
 sd.pause()
 
 # подсказка! для ускорения отрисовки можно
@@ -72,5 +64,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
