fc0b2d01ee861d0eeb6386a43da2427ba104c039
Irina Agafonova <la_caterina@mail.ru>
2020-03-15 10:28:53 +0300

    district done

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,8 +5,17 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from lesson_005.district.central_street.house1.room1 import folks as cent_st_h1_r1
+from lesson_005.district.central_street.house1.room2 import folks as cent_st_h1_r2
+from lesson_005.district.central_street.house2.room1 import folks as cent_st_h2_r1
+from lesson_005.district.central_street.house2.room2 import folks as cent_st_h2_r2
+from lesson_005.district.soviet_street.house1.room1 import folks as sov_st_h1_r1
+from lesson_005.district.soviet_street.house1.room2 import folks as sov_st_h1_r2
+from lesson_005.district.soviet_street.house2.room1 import folks as sov_st_h2_r1
+from lesson_005.district.soviet_street.house2.room2 import folks as sov_st_h2_r2
 
+district_residents = cent_st_h1_r1 + cent_st_h1_r2 + cent_st_h2_r1 + cent_st_h2_r2 + sov_st_h1_r1 + sov_st_h1_r2 + \
+                     sov_st_h2_r1 + sov_st_h2_r2
 
-
+print(', '.join(district_residents))
 
