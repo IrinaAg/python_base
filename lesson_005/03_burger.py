cafaadab6eaa4cf5c3b6a4f6164ba4137ee77f5f
Irina Agafonova <la_caterina@mail.ru>
2020-03-17 20:25:16 +0300

    burger done

--- lesson_005/03_burger.py
+++ lesson_005/03_burger.py
@@ -15,19 +15,8 @@
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-from lesson_005.my_burger import burger_buns
-from lesson_005.my_burger import cutlets
-from lesson_005.my_burger import cucumber
-from lesson_005.my_burger import tomato
-from lesson_005.my_burger import mayonnaise_sauce
-from lesson_005.my_burger import cheese
-from lesson_005.my_burger import mustard_sauce
-from lesson_005.my_burger import onion
-from lesson_005.my_burger import flavoring
-from lesson_005.my_burger import salad
-from lesson_005.my_burger import ketchup
-# TODO Здесь можно упростить импорт ещё сильнее
-# TODO можно использовать from .. import func1, func2, func3...
+from my_burger import burger_buns, cutlets, cucumber, tomato, mayonnaise_sauce, cheese, mustard_sauce, onion, flavoring, salad, ketchup
+
 # burger_buns()
 # cutlets()
 # cucumber()
