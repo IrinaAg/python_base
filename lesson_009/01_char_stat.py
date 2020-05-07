d7b838ac9cb5900f0caaa755237447a736994445
Irina Agafonova <la_caterina@mail.ru>
2020-05-07 15:48:58 +0300

    char stat TODO

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import zipfile
 
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
@@ -21,7 +22,108 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+
+class Statistics:
+
+    def __init__(self, file_name='voyna-i-mir.txt.zip'):
+        self.file_name = file_name
+        self.stat = {}
+        self.count = 0
+
+    def start_method(self):
+        self.unzip()
+        self.open()
+        self.sort()
+
+    def sort(self):
+        pass
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def open(self):
+        # stat = {}
+        # k = 0
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        self.count += 1
+                        if char in self.stat:
+                            self.stat[char] += 1
+                        else:
+                            self.stat[char] = 1
+            # print(stat)
+            # print(k)
+
+
+class Sorted(Statistics):
+    def sort(self):
+        print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+        print('|{name:^10}|'.format(name='буква'), '{key:^9}|'.format(key='частота'))
+        for i in sorted(self.stat.items(), key=lambda para: (para[1], para[0]), reverse=True):
+        # for i in sorted(self.stat.items(), key=lambda para: (para[1], para[0]), reverse=False):
+        # for i in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=True):
+        # for i in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=False):
+            print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+            print('|{name:^10}|'.format(name=i[0]), '{key:^9}|'.format(key=i[1]))
+        print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+        print('|{name:^10}|'.format(name='итого'), '{key:^9}|'.format(key=self.count))
+        print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+
+
+statis = Statistics(file_name='voyna-i-mir.txt.zip')
+statis1 = Sorted(Statistics)
+statis1.start_method()
+# TODO не могу понять почему вылезает ошибка при раззиповке
+# class Statistics:  TODO если один класс то все работает
+#
+#     def __init__(self, file_name):
+#         self.file_name = file_name
+#         self.stat = {}
+#         self.count = 0
+#
+#     def unzip(self):
+#         zfile = zipfile.ZipFile(self.file_name, 'r')
+#         for filename in zfile.namelist():
+#             zfile.extract(filename)
+#
+#     def open(self, file_name):
+#         # stat = {}
+#         # k = 0
+#         with open(file_name, 'r', encoding='cp1251') as file:
+#             for line in file:
+#                 for char in line:
+#                     if char.isalpha():
+#                         self.count += 1
+#                         if char in self.stat:
+#                             self.stat[char] += 1
+#                         else:
+#                             self.stat[char] = 1
+#             # print(stat)
+#             # print(k)
+#
+#     def sort(self):
+#         print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+#         print('|{name:^10}|'.format(name='буква'), '{key:^9}|'.format(key='частота'))
+#         # for i in sorted(stat.items(), key=lambda para: (para[1], para[0]), reverse=True):
+#         # for i in sorted(stat.items(), key=lambda para: (para[1], para[0]), reverse=False):
+#         # for i in sorted(stat.items(), key=lambda para: (para[0], para[1]), reverse=True):
+#         for i in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=False):
+#             print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+#             print('|{name:^10}|'.format(name=i[0]), '{key:^9}|'.format(key=i[1]))
+#         print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+#         print('|{name:^10}|'.format(name='итого'), '{key:^9}|'.format(key=self.count))
+#         print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+#
+#
+# statis = Statistics(file_name='voyna-i-mir.txt.zip')
+# statis.unzip()
+# statis.open(file_name='voyna-i-mir.txt')
+# statis.sort()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
