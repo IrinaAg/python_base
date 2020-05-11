17937463731b28ec4e115b20f90987f2234f31d6
Irina Agafonova <la_caterina@mail.ru>
2020-05-11 17:12:19 +0300

    files arrange TODO

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -40,54 +40,48 @@ class Extract:
 
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        self.time_name = []
 
     def extract(self):
+        self.time_file1 = []
+        self.time_name1 =[]
         for dirpath, dirnames, filenames in os.walk(folder_path):
-            # print('{dirpath:-^40}'.format(dirpath=dirpath))
             for file in filenames:
                 self.time_file = os.path.join(dirpath, file)
+                self.time_file1.append(self.time_file)
                 self.name = os.path.getmtime(self.time_file)
                 self.time_name = time.gmtime(self.name)
-                # print(self.time_name[])
-                # print('{file}||{time_name}'.format(file=file, time_name=time_name))
-                # TODO Проблема с одним файлом заключается в том, что проходите вы по всем файлам
-                # TODO А далее вызываете метод create и replication отдельно, вне цикла.
-                # TODO Вот они и срабатывают по разу.
+                self.time_name1.append(self.time_name)
 
     def create(self):
-        print(self.time_name)
-        self.folder_name = '{tm_year}/{tm_mon}'.format(tm_year=self.time_name.tm_year, tm_mon=self.time_name.tm_mon)
-        if len(self.folder_name) <= 6:
-            self.folder_name = self.folder_name.replace('/', '/0')
-        self.new_path = os.path.join(path, self.folder_name)
-
-        if not os.path.exists(self.new_path):
-            os.makedirs(self.new_path)  # TODO Тут можно использовать параметр exist_ok=True вместо проверки
-            # TODO на наличие папки
+        self.new_path1 = []
+        for self.time_name in self.time_name1:
+            self.folder_name = '{tm_year}/{tm_mon}'.format(tm_year=self.time_name.tm_year,
+                                                           tm_mon=self.time_name.tm_mon)
+            if len(self.folder_name) <= 6:
+                self.folder_name = self.folder_name.replace('/', '/0')
+            self.new_path = os.path.join(path, self.folder_name)
+            self.new_path1.append(self.new_path)
+            os.makedirs(self.new_path, exist_ok=True)
 
     def replication(self):
-        print(self.time_file)  # TODO обратите внимание, что в этой переменной уже icons\status\weather-storm.png
-        old_image_path = os.path.join(folder_path, self.time_file)
-        new_image_path = os.path.join(self.new_path)
-
-        print(old_image_path, new_image_path)  # TODO И новый путь странно формируется icons_by_year\2018/03
-        # TODO Вероятно у вас другая ОС, в этом случае путь нужно нормализовать при помощи os.path.normpath
-        print(os.path.normpath(self.new_path))
-        shutil.copy2(old_image_path, new_image_path)
+        for self.time_file in self.time_file1:
+            self.old_image_path = os.path.join(self.time_file)
+            # print(self.time_file)
+            for self.new_path in self.new_path1: #TODO теперь все файлы в каждую папку переносятся не могу понять как сделать привавильно
+                #TODO или я не в том направлении иду
+                self.new_image_path = os.path.join(os.path.normpath(self.new_path))
+                # print(self.new_path)
+                shutil.copy2(self.old_image_path, self.new_image_path)
 
 
 folder_path = 'icons'
 path = 'icons_by_year'
-# TODO Если закомментировать эти пути, то видно, что в классе вы используете внешние переменные
-# TODO Делать этого не нужно, передавайте их в объект класса переменными
 time_name = Extract(folder_path)
 time_name.extract()
 time_name.create()
 time_name.replication()
 
-# изначально делала не на классах, все работает, но на классах только один файл перемещает
-# TODO
+
 # for dirpath, dirnames, filenames in os.walk(folder_path):
 #     # print('{dirpath:-^40}'.format(dirpath=dirpath))
 #     for file in filenames:
@@ -101,7 +95,7 @@ time_name.replication()
 #         new_path = os.path.join(path, folder_name)
 #         if not os.path.exists(new_path):
 #             os.makedirs(new_path)
-#         old_image_path = os.path.join(folder_path, time_file)
+#         old_image_path = os.path.join(time_file)
 #         new_image_path = os.path.join(new_path)
 #         shutil.copy2(old_image_path, new_image_path)
 
