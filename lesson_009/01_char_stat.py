3f75b0cc4612d173ef8f25e16131f4fc73593603
Irina Agafonova <la_caterina@mail.ru>
2020-05-11 19:39:41 +0300

    char stat done

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -34,9 +34,25 @@ class Statistics:
     def start_method(self):
         self.unzip()
         self.open()
-        self.sort()
+        self.sort_requency_descending()
+        self.sort_frequency_ascending()
+        self.sort_alphabetically_ascending()
+        self.sort_alphabetically_descending()
+        self.print_data()
 
-    def sort(self):
+    def sort_requency_descending(self):
+        pass
+
+    def sort_frequency_ascending(self):
+        pass
+
+    def sort_alphabetically_ascending(self):
+        pass
+
+    def sort_alphabetically_descending(self):
+        pass
+
+    def print_data(self):
         pass
 
     def unzip(self):
@@ -46,8 +62,6 @@ class Statistics:
         self.file_name = filename
 
     def open(self):
-        # stat = {}
-        # k = 0
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
                 for char in line:
@@ -57,33 +71,51 @@ class Statistics:
                             self.stat[char] += 1
                         else:
                             self.stat[char] = 1
-            # print(stat)
-            # print(k)
 
 
 class Sorted(Statistics):
-    # TODO Тут нужно вынести принт в отдельный метод.
-    # TODO И сортировку тоже. Метод сортировки может например изменять атрибут объекта self.sort_data
-    # TODO А потом можно вызывать метод print_data, который распечатает данные из self.sort_data
-    def sort(self):
+
+    def __init__(self):
+        super().__init__()
+        self.sort_data = []
+
+    def sort_requency_descending(self):
+        for sort_data in sorted(self.stat.items(), key=lambda para: (para[1], para[0]), reverse=True):
+            self.sort_data.append(sort_data)
+
+    def sort_frequency_ascending(self):
+        for sort_data in sorted(self.stat.items(), key=lambda para: (para[1], para[0]), reverse=False):
+            self.sort_data.append(sort_data)
+
+    def sort_alphabetically_ascending(self):
+        for sort_data in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=True):
+            self.sort_data.append(sort_data)
+
+    def sort_alphabetically_descending(self):
+        for sort_data in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=False):
+            self.sort_data.append(sort_data)
+
+
+class Print_data(Sorted):
+
+    def print_data(self):
         print('+' + '-' * 10 + '+' + '-' * 10 + '+')
         print('|{name:^10}|'.format(name='буква'), '{key:^9}|'.format(key='частота'))
-        for i in sorted(self.stat.items(), key=lambda para: (para[1], para[0]), reverse=True):
-            # TODO Нужно будет для каждого типа сортировки свой метод сортировки переопределить
-            # for i in sorted(self.stat.items(), key=lambda para: (para[1], para[0]), reverse=False):
-            # for i in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=True):
-            # for i in sorted(self.stat.items(), key=lambda para: (para[0], para[1]), reverse=False):
-            print('+' + '-' * 10 + '+' + '-' * 10 + '+')
-            print('|{name:^10}|'.format(name=i[0]), '{key:^9}|'.format(key=i[1]))
         print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+        for sort_data in self.sort_data:
+            print('|{name:^10}|'.format(name=sort_data[0]), '{key:^9}|'.format(key=sort_data[1]))
+            print('+' + '-' * 10 + '+' + '-' * 10 + '+')
         print('|{name:^10}|'.format(name='итого'), '{key:^9}|'.format(key=self.count))
         print('+' + '-' * 10 + '+' + '-' * 10 + '+')
 
 
 statis = Statistics(file_name='voyna-i-mir.txt.zip')
-statis1 = Sorted()  # TODO Тут не нужно указывать дополнительно класс-родитель, он уже указан в class Sorted(Statistics)
-statis1.start_method()
-# не могу понять почему вылезает ошибка при раззиповке
+statis1 = Sorted()
+statis2 = Print_data()
+statis2.start_method()
+
+
+
 # class Statistics:
 #
 #     def __init__(self, file_name):
