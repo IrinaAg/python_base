6e4fc73eff5af97fcc1329caef58dca502bdaea0
Irina Agafonova <la_caterina@mail.ru>
2020-05-12 19:22:50 +0300

    log parser done

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -27,22 +27,10 @@ class Read:
         self.lines = {}
 
     def start_method(self):
-        self.minute()
-        self.hour()
-        self.month()
-        self.year()
+        self.sorted()
         self.record()
 
-    def minute(self):
-        pass
-
-    def hour(self):
-        pass
-
-    def month(self):
-        pass
-
-    def year(self):
+    def sorted(self):
         pass
 
     def record(self):
@@ -52,80 +40,49 @@ class Read:
         file.close()
 
 
-class Minute(Read):  # TODO Та же проблема, что и в прошлом задании
-    # TODO Должен быть один метод, переопределенный в наследниках
-    # TODO Но тут даже метод не нужно переопределять, надо только атрибут переопределить
-    part = 17  # TODO Кстати раз это константа, можно название написать заглавными буквами
+class Minute(Read):
+    PART = 17
 
-    def minute(self, file_name='events.txt'):
+    def sorted(self, file_name='events.txt'):
         with open(file_name, 'r', encoding='cp1251') as file:
             for line in file:
                 if 'NOK' in line:
-                    char = line[1:self.part]
+                    char = line[1:self.PART]
                     if char in self.lines:
                         self.lines[char] += 1
                     else:
                         self.lines[char] = 1
 
 
-class Hour(Read):
-    part = 14
-
-    def hour(self, file_name='events.txt'):
-        with open(file_name, 'r', encoding='cp1251') as file:
-            for line in file:
-                if 'NOK' in line:
-                    char = line[1:self.part]
-                    if char in self.lines:
-                        self.lines[char] += 1
-                    else:
-                        self.lines[char] = 1
+class Hour(Minute):
+    PART = 14
 
 
-class Month(Read):
-    part = 8
-
-    def month(self, file_name='events.txt'):
-        with open(file_name, 'r', encoding='cp1251') as file:
-            for line in file:
-                if 'NOK' in line:
-                    char = line[1:self.part]
-                    if char in self.lines:
-                        self.lines[char] += 1
-                    else:
-                        self.lines[char] = 1
+class Month(Minute):
+    PART = 8
 
 
-class Year(Read):
-    part = 5
+class Year(Minute):
+    PART = 5
 
-    def year(self, file_name='events.txt'):
-        with open(file_name, 'r', encoding='cp1251') as file:
-            for line in file:
-                if 'NOK' in line:
-                    char = line[1:self.part]
-                    if char in self.lines:
-                        self.lines[char] += 1
-                    else:
-                        self.lines[char] = 1
 
 
 read_file = Read(file_name='events.txt')
-# read_file1 = Minute(Read)
+read_file1 = Minute(Read)
 # read_file1.start_method()
 
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
-# read_file2 = Hour(Read)
-# read_file2.start_method()
+read_file2 = Hour(Minute)
+read_file2.start_method()
 
 #  - по месяцу
-read_file3 = Month(Read)
-read_file3.start_method()
+read_file3 = Month(Minute)
+# read_file3.start_method()
 
 #  - по году
-# read_file4 = Year(Read)
+read_file4 = Year(Minute)
 # read_file4.start_method()
 
 # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
