d3f813bef108e3f9237ac776efdb7dce9c43860a
Irina Agafonova <la_caterina@mail.ru>
2020-06-04 21:02:12 +0300

    prime numbers done

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -6,13 +6,14 @@
 
 def get_prime_numbers(n):
     prime_numbers = []
-    for number in range(2, n+1):
+    for number in range(2, n + 1):
         for prime in prime_numbers:
             if number % prime == 0:
                 break
         else:
             prime_numbers.append(number)
     return prime_numbers
+
 
 # for number in get_prime_numbers(n=100):
 #     print(number)
@@ -36,28 +37,22 @@ class PrimeNumbers:
         return self
 
     def __next__(self):
-        # TODO цикл от 2 до self.n+1 не подходит
-        # TODO Иначе мы каждый раз будем начинать цикл с самого начала.
-        # TODO попробуйте либо начинать от прошлого найденного простого числа, либо испольуйте while
-        for number in range(2, self.n + 1):
-            for prime in self.prime_numbers:
-                if number % prime == 0:
-                    break
-            else:
-                self.prime_numbers.append(number)
-                return number
-            self.i += 1
-        if self.i > self.n:  # TODO Эту проверку я бы выше поставил
+        self.i += 1
+        if self.i >= self.n:
             raise StopIteration()
+        while self.i <= self.n:
+            for number in range(2, self.n + 1):
+                for prime in self.prime_numbers:
+                    if number % prime == 0:
+                        break
+                else:
+                    self.prime_numbers.append(number)
+                    return number
 
 
-
-# TODO Не забывайте про CTRL+ALT+L
 prime_number_iterator = PrimeNumbers(n=10000)
 for number in prime_number_iterator:
     print(number)
-
-
 
 # TODO после подтверждения части 1 преподователем, можно делать
 # Часть 2
