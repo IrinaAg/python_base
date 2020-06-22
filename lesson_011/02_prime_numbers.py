215a9015571fc2d0d6e70abfda5e48a785870a20
Irina Agafonova <la_caterina@mail.ru>
2020-06-22 21:02:06 +0300

    prime numbers done

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -70,16 +70,20 @@ def get_prime_numbers(i):
             return False
     return True
 
+#
+# def prime_numbers_generator(i=1, n=10000):
+#     while i < n:
+#         if get_prime_numbers(i):
+#             yield i
+#         i += 1
 
-def prime_numbers_generator(i=1, n=10000):
-    while i < n:
-        if get_prime_numbers(i):
-            yield i
-        i += 1
 
+# prime_number_generator = PrimeNumbers(n=10000)
+# for number in prime_number_generator:
+#     print(number)
 
-for number_g, number_i in zip(prime_numbers_generator(n=10000), prime_number_iterator):
-    print(number_g == number_i)
+# for number_g, number_i in zip(prime_numbers_generator(n=10000), prime_number_iterator):
+#     print(number_g == number_i)
 
 
 # Часть 3
@@ -89,37 +93,23 @@ for number_g, number_i in zip(prime_numbers_generator(n=10000), prime_number_ite
 #       то для вычисления "счастливости" брать равное количество цифр с начала и конца:
 #           727 -> 7(2)7 -> 7 == 7 -> True
 #           92083 -> 92(0)83 -> 9+2 == 8+3 -> True
-def get_prime_numbers(i):  # TODO А заемэта функция дублируется тут и ниже?
-    if i == 1:
-        return False
-    for x in range(2, i):
-        if i % x == 0:
-            return False
-    return True
 
 
 def sum_digits(i):
     middle = len(str(i)) // 2
-    a = str(i)[:1]  # TODO А зачем нужны все эти переменные?
-    b = str(i)[1:2]
-    c = str(i)[2:3]
-    d = str(i)[3:4]
     if middle == 0 or str(i)[:middle] == str(i)[-middle:]:
         return True
-    for _ in range(i):
-        if int(a) + int(b) == int(c) + int(d):
-            # не могу посчитать правую и левую часть числа
-            # TODO Тут можно использовать lambda функцию, или функции sum, list, map, int
-            # TODO Разделить строку на элементы, к каждому применить Int а затем всё сложить
-            # TODO Но делать это над срезами, а не над отдельными переменными
-            print(int(a) + int(b))
-            print(int(c) + int(d))
-        return True
+    if len(str(i)) > 3:
+        for _ in range(i):
+            num = list(str(i))
+            if int(num[0]) + int(num[1]) == int(num[2]) + int(num[3]):
+                return True
     else:
         return False
 
 
-def prime_numbers_generator(i=1, n=10000):  # TODO Попробуйте передать функцию для проверки параметром
+
+def prime_numbers_generator(i=1, n=10000, sum_digits=sum_digits, get_prime_numbers=get_prime_numbers):
     while i < n:
         if get_prime_numbers(i) and sum_digits(i):
             yield i
@@ -132,32 +122,19 @@ for number in prime_numbers_generator(n=10000):
 
 # 2) "палиндромное" - одинаково читающееся в обоих направлениях. Например 723327 и 101
 
-
-def get_prime_numbers(i):
-    if i == 1:
-        return False
-    for x in range(2, i):
-        if i % x == 0:
-            return False
-    return True
-
-
-def palindrom(i):
-    if str(i) == str(i)[::-1]:  # TODO Можно сократить до 1 строки - оставив в return само это условие
-        return True
-    else:
-        return False
-
-
-def prime_numbers_generator(i=1, n=10000):
-    while i < n:
-        if get_prime_numbers(i) and palindrom(i):
-            yield i
-        i += 1
-
-
-for number in prime_numbers_generator(n=10000):
-    print(number)
+# def palindrom(i):
+#         return str(i) == str(i)[::-1]
+#
+#
+# def prime_numbers_generator(i=1, n=10000):
+#     while i < n:
+#         if get_prime_numbers(i) and palindrom(i):
+#             yield i
+#         i += 1
+#
+#
+# for number in prime_numbers_generator(n=10000):
+#     print(number)
 
 # 3) придумать свою (https://clck.ru/GB5Fc в помощь)
 #
