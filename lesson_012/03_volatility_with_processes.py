d2c04650dae22a1dd4afce76d1d3e6a2f70eee40
Irina Agafonova <la_caterina@mail.ru>
2020-07-10 22:10:52 +0300

    volatility with processes done

--- lesson_012/03_volatility_with_processes.py
+++ lesson_012/03_volatility_with_processes.py
@@ -17,6 +17,72 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/02_volatility_with_threads.py !!!
 
-# TODO тут ваш код в многопроцессном стиле
+import multiprocessing
+from os import listdir, path
+from utils import time_track, VolatilityPrint
+from queue import Empty
+
+
+store = {}
+trades = {}
+file_path = 'trades'
+
+
+class TickerVolatility(multiprocessing.Process):
+
+    def __init__(self, file_name, collector, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_name = file_name
+        self.collector = collector
+
+    def run(self):
+        ticker_name = path.basename(self.file_name).split('_')[1].split('.')[0]
+        results = []
+        with open(self.file_name, 'r') as f:
+            for line in f:
+                if line.startswith('SECID'):
+                    continue
+                else:
+                    results.append(float(line.split(',')[2]))
+        results = sorted(results)
+        half_sum = (results[0] + results[-1]) / 2
+        volatility = (results[-1] - results[0]) / half_sum * 100
+        # store[ticker_name] = (round(volatility, 2))
+        self.collector.put({ticker_name: round(volatility, 2)})
+        # sizers=[TickerVolatility(file_name=self.file_name, collector=self.collector)]
+        # while True:
+        #     try:
+        #         ticker = self.collector.get(timeout=0.1)
+        #         trades.update(ticker)
+        #     except Empty:
+        #         if not any(sizer.is_alive() for sizer in sizers):
+        #             break
+
+
+@time_track
+def main():
+    collector = multiprocessing.Queue()
+    sizers = [TickerVolatility(path.join(file_path, file_name), collector=collector) for file_name in listdir(file_path)]
+
+    for sizer in sizers:
+        sizer.start()
+
+    while True:
+        try:
+            ticker = collector.get(timeout=0.1)
+            trades.update(ticker)
+        except Empty:
+            if not any(sizer.is_alive() for sizer in sizers):
+                break
+
+    for sizer in sizers:
+        sizer.join()
+
+    proc = VolatilityPrint(trades)
+    proc.print()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ lesson_012/utils.py
@@ -0,0 +1,32 @@
+import time
+
+
+def time_track(func):
+    def surrogate(*args, **kwargs):
+        started_at = time.time()
+
+        result = func(*args, **kwargs)
+
+        ended_at = time.time()
+        elapsed = round(ended_at - started_at, 4)
+        print(f'Функция работала {elapsed} секунд(ы)')
+        return result
+    return surrogate
+
+
+class VolatilityPrint:
+    def __init__(self, store):
+        self.store = store
+
+    def print(self):
+        print('Максимальная волатильность:')
+        view = sorted(self.store.items(), key=lambda elem: elem[1], reverse=True)
+        for ticker, value in view[:3]:
+            print(f'       {ticker} - {value}%')
+        print('Минимальная волатильность:')
+        view = [(ticker, vol) for ticker, vol in view if vol > 0]
+        for ticker, value in view[-3:]:
+            print(f'       {ticker} - {value}%')
+        print('Нулевая волатильность:')
+        zeroes = [ticker for ticker, vol in self.store.items() if vol == 0]
+        print(f'       {", ".join(sorted(zeroes))}')
