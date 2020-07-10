9284cc6383b1d55746bc8b9b86539cc31143de00
Irina Agafonova <la_caterina@mail.ru>
2020-07-10 22:06:59 +0300

    volatility with threads done

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -16,7 +16,55 @@
 #   Нулевая волатильность:
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
-#
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/01_volatility.py !!!
 
-# TODO тут ваш код в многопоточном стиле
+import threading
+from os import listdir, path
+from utils import time_track, VolatilityPrint
+import time
+
+store = {}
+trades = {}
+file_path = 'trades'
+
+
+class TickerVolatility(threading.Thread):
+
+    def __init__(self, file_name, *args, **kwargs):
+        super().__init__(**kwargs)
+        self.file_name = file_name
+
+
+    def run(self):
+        ticker_name = path.basename(self.file_name).split('_')[1].split('.')[0]
+        results = []
+        with open(self.file_name, 'r') as f:
+            for line in f:
+                if line.startswith('SECID'):
+                    continue
+                else:
+                    results.append(float(line.split(',')[2]))
+        results = sorted(results)
+        half_sum = (results[0] + results[-1]) / 2
+        volatility = (results[-1] - results[0]) / half_sum * 100
+        store[ticker_name] = (round(volatility, 2))
+
+
+@time_track
+def main():
+    sizers = [TickerVolatility(path.join(file_path, file_name)) for file_name in listdir(file_path)]
+
+    for sizer in sizers:
+        sizer.start()
+        # time.sleep(0.001)
+        if sizer.is_alive():
+            sizer.need_stop = True
+    for sizer in sizers:
+        sizer.join()
+
+    proc = VolatilityPrint(store)
+    proc.print()
+
+
+if __name__ == '__main__':
+    main()
+
