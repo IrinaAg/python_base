c6f08e39909a941babab3d7212e39b18af11fc06
Irina Agafonova <la_caterina@mail.ru>
2020-08-05 23:59:44 +0300

    ticket done

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import argparse
+import os
 from PIL import Image, ImageDraw, ImageFont, ImageColor
+
 
 # Заполнить все поля в билете на самолет.
 # Создать функцию, принимающую параметры: ФИО, откуда, куда, дата вылета,
@@ -23,54 +25,82 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 # и заполнять билет.
 
 
-class TicketMaker:
+# class TicketMaker:
+#
+#     def __init__(self, template=None, font_path=None):
+#         self.template = "images/ticket_template.png" if template is None else template
+#         if font_path is None:
+#             self.font_path = "python_snippets/fonts/Bressay Display.ttf"
+#         else:
+#             self.font_path = font_path
+#
+#     def make_ticket(self, save_to=None):
+#         im = Image.open(self.template)
+#         draw = ImageDraw.Draw(im)
+#         font = ImageFont.truetype(self.font_path, size=16)
+#
+#         y = im.size[1] - 225 - (10 + font.size) * 2
+#         fio = f"ИВАНОВ И.И."
+#         draw.text((50, y), fio, font=font, fill=ImageColor.colormap['black'])
+#
+#         y = im.size[1] - 190 - font.size
+#         from_ = f"ЗЕМЛЯ"
+#         draw.text((50, y), from_, font=font, fill=ImageColor.colormap['black'])
+#
+#         y = im.size[1] - 125 - font.size
+#         to = f"ЛУНА"
+#         draw.text((50, y), to, font=font, fill=ImageColor.colormap['black'])
+#
+#         y = im.size[1] - 125 - font.size
+#         date = f"09.12"
+#         draw.text((290, y), date, font=font, fill=ImageColor.colormap['black'])
+#
+#         # im.show()
+#         save_to = save_to if save_to else 'ticket_image.png'
+#         dir_path = "/Users/agafonova/python_base/lesson_013/test"
+#         os.makedirs(dir_path, exist_ok=True)
+#         file_path = os.path.join(dir_path, save_to)
+#         im.save(file_path)
+#         print(f'Ticket saved az {save_to}')
+#
+#
+# if __name__ == '__main__':
+#     maker = TicketMaker()
+#     maker.make_ticket()
 
-    def __init__(self, template=None, font_path=None):
-        # TODO Пути старайтесь указывать относительно рабочей директории (той, в которой лежит главный запускаемый файл)
-        # TODO т.к. здесь у нас проект состоит из нескольких "мини"-проектов, то можно выполнить хитрый приём, явно указав
-        # TODO на рабочую директорию.
-        # TODO Сделать это можно либо в Run - Edit configurations
-        # TODO Либо можно просто выделить нужную папку как source root
-        # TODO для этого надо нажать на неё правой кнопкой - mark directory as - source root
-        self.template = "/Users/agafonova/python_base/lesson_013/images/ticket_template.png" if template is None else template
-        if font_path is None:
-            self.font_path = "/Users/agafonova/python_base/lesson_013/python_snippets/fonts/Bressay Display.ttf"
-            # TODO загрузите свой шрифт в репозиторий пожалуйста
-        else:
-            self.font_path = font_path
+# Усложненная часть с argparse
 
-    def make_ticket(self, save_to=None):
-        im = Image.open(self.template)
-        draw = ImageDraw.Draw(im)
-        font = ImageFont.truetype(self.font_path, size=16)
 
-        y = im.size[1] - 225 - (10 + font.size) * 2
-        fio = f"ИВАНОВ И.И."
-        draw.text((50, y), fio, font=font, fill=ImageColor.colormap['black'])
+def make_ticket():
+    im = Image.open("/Users/agafonova/python_base/lesson_013/images/ticket_template.png")
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype("/Users/agafonova/python_base/lesson_013/python_snippets/fonts/Bressay Display.ttf",
+                              size=16)
+    parser = argparse.ArgumentParser(description='Заполнение билета')
+    parser.add_argument('fio', type=str, help='фамилия')
+    parser.add_argument('from_', type=str, help='откуда летим')
+    parser.add_argument('to', type=str, help='куда летим')
+    parser.add_argument('date', type=str, help='когда летим')
+    args = parser.parse_args()
+    y = im.size[1] - 225 - (10 + font.size) * 2
+    draw.text((50, y), args.fio, font=font, fill=ImageColor.colormap['black'])
 
-        y = im.size[1] - 190 - font.size
-        from_ = f"ЗЕМЛЯ"
-        draw.text((50, y), from_, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 190 - font.size
+    draw.text((50, y), args.from_, font=font, fill=ImageColor.colormap['black'])
 
-        y = im.size[1] - 125 - font.size
-        to = f"ЛУНА"
-        draw.text((50, y), to, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 125 - font.size
+    draw.text((50, y), args.to, font=font, fill=ImageColor.colormap['black'])
 
-        y = im.size[1] - 125 - font.size
-        date = f"09.12"
-        draw.text((290, y), date, font=font, fill=ImageColor.colormap['black'])
-
-        im.show()
-        save_to = save_to if save_to else 'ticket_image.png'
-        # TODO Сохранение изображений я бы советовал выполнять в отдельную папку
-        # TODO А её наличие стоит проверить и, при необходимости, создать эту папку
-        im.save(save_to)
-        print(f'Post card saved az {save_to}')
+    y = im.size[1] - 125 - font.size
+    draw.text((290, y), args.date, font=font, fill=ImageColor.colormap['black'])
+    im.show()
+    parser.add_argument('-s', '--save_to', help='путь для сохранения заполненнего билета')
+    save_to = 'ticket_image.png'
+    dir_path = "/Users/agafonova/python_base/lesson_013/test"
+    os.makedirs(dir_path, exist_ok=True)
+    file_path = os.path.join(dir_path, save_to)
+    im.save(file_path)
 
 
 if __name__ == '__main__':
-    maker = TicketMaker()
-    maker.make_ticket()
-
-# TODO В основном всё верно, попробуете усложненную часть с argparse?
-# TODO на всякий случай оставлю полезный источник https://habr.com/ru/post/144416/
+    make_ticket()
