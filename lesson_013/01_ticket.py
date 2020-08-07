db517e9a6918ffe7c88c3b2e4992c01c4cf476d5
Irina Agafonova <la_caterina@mail.ru>
2020-08-07 16:49:14 +0300

    ticket done

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -71,41 +71,38 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 # Усложненная часть с argparse
 
 
-def make_ticket():
-    # TODO 1) Пути надо указать относительно директории lesson_013
-    im = Image.open("images/ticket_template.png")  # TODO Примерно так
+def make_ticket(fio, from_, to, date):
+    im = Image.open("lesson_013/images/ticket_template.png")
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype("/Users/agafonova/python_base/lesson_013/python_snippets/fonts/Bressay Display.ttf",
                               size=16)
-    # TODO Аргпарсер должен быть вне функции
-    # TODO Это отдельный элемент программы, который может быть одним из вариантов запуска вашей функции
-    # TODO Его можно выделить или в отдельную функцию, или просто записать в if __name__ == '__main__'
+    y = im.size[1] - 225 - (10 + font.size) * 2
+    draw.text((50, y), fio, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 190 - font.size
+    draw.text((50, y), from_, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 125 - font.size
+    draw.text((50, y), to, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 125 - font.size
+    draw.text((290, y), date, font=font, fill=ImageColor.colormap['black'])
+    # im.show()
+    save_to = 'ticket_image.png'
+    dir_path = "lesson_013/test"
+    os.makedirs(dir_path, exist_ok=True)
+    file_path = os.path.join(dir_path, save_to)
+    im.save(file_path)
+
+
+def command_line():
     parser = argparse.ArgumentParser(description='Заполнение билета')
     parser.add_argument('fio', type=str, help='фамилия')
     parser.add_argument('from_', type=str, help='откуда летим')
     parser.add_argument('to', type=str, help='куда летим')
     parser.add_argument('date', type=str, help='когда летим')
     args = parser.parse_args()
-    # TODO А уже потом передавать все вот эти аргументы в функцию, как при обычном вызове
-    y = im.size[1] - 225 - (10 + font.size) * 2
-    draw.text((50, y), args.fio, font=font, fill=ImageColor.colormap['black'])
-
-    y = im.size[1] - 190 - font.size
-    draw.text((50, y), args.from_, font=font, fill=ImageColor.colormap['black'])
-
-    y = im.size[1] - 125 - font.size
-    draw.text((50, y), args.to, font=font, fill=ImageColor.colormap['black'])
-
-    y = im.size[1] - 125 - font.size
-    draw.text((290, y), args.date, font=font, fill=ImageColor.colormap['black'])
-    im.show()
     parser.add_argument('-s', '--save_to', help='путь для сохранения заполненнего билета')
-    save_to = 'ticket_image.png'
-    dir_path = "/Users/agafonova/python_base/lesson_013/test"  # TODO И тут путь поправить
-    os.makedirs(dir_path, exist_ok=True)
-    file_path = os.path.join(dir_path, save_to)
-    im.save(file_path)
+
+    make_ticket(args.fio, args.from_, args.to, args.date)
 
 
 if __name__ == '__main__':
-    make_ticket()
+    command_line()
